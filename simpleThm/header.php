5d873d2708d4691ac217c0ce8bea01c4029b3643
BoodziooPL <bogdanmalik@gmail.com>
2022-12-06 01:37:38 +0100

    add stylesheet

--- simpleThm/header.php
+++ simpleThm/header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php bloginfo('language');?>">
 <head>
- <meta charset="UTF-8">
+ <meta charset="<?php bloginfo('charset');?>">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <meta name="viewport" content="width=\, initial-scale=1.0">
  <title><?php bloginfo();?></title>
+ <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
 </head>
 <body>

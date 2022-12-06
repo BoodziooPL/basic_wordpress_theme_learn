2766a4e4ebcabe1fba5dec3328a32bc4b2703a98
BoodziooPL <bogdanmalik@gmail.com>
2022-12-06 01:29:19 +0100

    new files add

--- simpleThm/index.php
+++ simpleThm/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
- <meta charset="UTF-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>My First WP Theme</title>
-<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-</head>
-<body>
- <h1>Hello</h1>
-</body>
-</html>
+
+<?php get_header(); ?>
+
+<?php get_footer(); ?>

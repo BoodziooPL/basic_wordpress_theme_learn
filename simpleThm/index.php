146ba6a0b411cb56db76530d2806bf14a0f4b95b
B00210 <bogdanmalik@gmail.com>
2022-12-08 21:47:42 +0100

    if and while

--- simpleThm/index.php
+++ simpleThm/index.php
@@ -1,4 +1,21 @@
 
 <?php get_header(); ?>
+<?php 
+//Loops
+    if(have_posts()){
+        while(have_posts(  )){
+            the_post(  );
+            the_content();
+        }
+    }
+
+    //  if(have_posts()) :
+    //     while(have_posts(  )) :
+    //         the_post(  );
+    //         the_content();
+    //     Endwhile;
+    // Endif;
+    // 2 mozliwe opcje, wp pozwala na uzywanie : zamiast {} i konczenie "endif or endwhile etc.. etc.. 
+?>
 
 <?php get_footer(); ?>

b8aca504b129fc4cd9e868f7549823fee31b60a7
B00210 <bogdanmalik@gmail.com>
2022-12-09 23:51:00 +0100

    add functions and custom js & css

---
+++ simpleThm/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<section>
+    <h1>Hello This Page not found</h1>
+    <p>to back home click : <a href="<?php bloginfo('url');?>">Powrót</a></p>
+</section>
+
+
+<?php get_footer(); ?>
---
+++ simpleThm/functions.php
@@ -0,0 +1,34 @@
+<?php
+//## funkcja do ustawienia np. tytuly strony, mozna dodac rowniez custom-header parametry ustawiamy w add_theme_support
+function theme_setup(){
+    add_theme_support( 'title-tag' );
+}
+// ## odpalenie funkcji wyżej, odpalana podczas ladowania strony po zainicjowaniu motywu.  
+add_action( 'after_setup_theme','theme_setup');
+
+
+//## poniższa funkcja dodaje style.css oraz dodatkowe cssy i js'a na stronę
+function add_scripts( ){
+    //## implementacja głównego CSS'a
+    wp_enqueue_style( 'main-stylesheet',get_stylesheet_uri( ) );
+    
+    
+    //## dodajemy dodatkowy css
+    wp_enqueue_style('magic', get_template_directory_uri('/css.animation.css'));
+
+    // ## dodajemy js'a
+    wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js');
+}
+//## odpalenie powyzszej funkcji 2 parametry wp_enqueue_script dla wp a drugi to nazwa funkcji
+add_action('wp_enqueue_scripts','add_scripts');
+
+
+
+
+//## funkcja, zwracajaca liczbę, dla "excerpt_length, ktory zmienia długość słow w wyswietlanym poscie."
+function custom_excerpt(){
+    return 15;
+}
+//## odpalenie funkcji "custom_excerpt"
+add_action('excerpt_length','custom_excerpt')
+?>

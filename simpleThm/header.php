a623bb123d66a2fe6ef7b2406b5628a02fe272b1
B00210 <bogdanmalik@gmail.com>
2022-12-09 02:42:00 +0100

    wrap post content

--- simpleThm/header.php
+++ simpleThm/header.php
@@ -5,8 +5,11 @@
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <meta name="viewport" content="width=\, initial-scale=1.0">
  <title><?php bloginfo('name');?></title>
-
+              
+          <!-- style.css -->
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+
+          <!-- dodanie skryptow wordpressa -->
 <?php wp_head(  ) ?>
 </head>
 
--- simpleThm/index.php
+++ simpleThm/index.php
@@ -3,25 +3,38 @@
 <?php get_header(); ?>
 
 <section class="post-area">
+
 <!-- petla dodająca posty na stronę -->
 <?php 
-//Loops
+//Petla iterujaca po wszystkich postach dostepnych w bazie danych
     if(have_posts()){
         while(have_posts(  )){
-            the_post(  );
-            the_title( );
-            the_content();
+            the_post(  );?>
+<!-- tworzenie na podstawie postow article, div etc... dla DOM  -->
+            
+        <article <?php post_class();?> id="post-<?php the_ID(); ?>">
+            <h1><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h1>
+        <div class="post-meta">
+           <p>Posted on: <?php the_date(); ?> at: <?php the_time(); ?></p>
+           <p>Posted by: <?php the_author( ); ?></p>   
+        </div>
+<!-- kod dodajacy czesc kontentu na strone pod powyzszym tytulem postu, autorem oraz datą -->
+        <?php the_excerpt() ?>
+        </article>
+        
+<!-- zamkniecie petli, nawiasy zamykają powyzszego while i if'a -->
+        <?php 
         }
     }
-
-    //  if(have_posts()) :
-    //     while(have_posts(  )) :
-    //         the_post(  );
-    //          the_title( );
-    //         the_content();
-    //     Endwhile;
-    // Endif;
-    // 2 mozliwe opcje, wp pozwala na uzywanie : zamiast {} i konczenie "endif or endwhile etc.. etc.. 
+            // 2 mozliwe opcje, wp pozwala na uzywanie : zamiast {} i konczenie "endif or endwhile etc.. etc.. 
+            //  if(have_posts()) :
+            //     while(have_posts(  )) :
+            //         the_post(  );
+            //          the_title( );
+            //         the_content();
+            //     Endwhile;
+            // Endif;
+// zamkniecie pętli   
 ?>
 
 </section>
